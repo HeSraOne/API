35406b102a26fe59d06d56b919801f65a7fa9a6b
HeSraOne <141596127+HeSraOne@users.noreply.github.com>
2023-08-10 11:26:32 +0700

    Add files via upload

---
+++ bootstrap/cache/s_p_p_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\SPP\\Providers\\SPPServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\SPP\\Providers\\SPPServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
